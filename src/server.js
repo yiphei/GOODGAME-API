34956f48160ddee9282e8c81a80e7c8684fc2edd
savannah <savannah.m.liu.18@dartmouth.edu>
2018-05-15 09:50:41 -0400

    node update

--- src/server.js
+++ src/server.js
@@ -6,12 +6,6 @@ import morgan from 'morgan';
 import mongoose from 'mongoose';
 import apiRouter from './router';
 
-// DB Setup
-const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
-mongoose.connect(mongoURI);
-// set mongoose promises to es6 default
-mongoose.Promise = global.Promise;
-
 // initialize
 const app = express();
 
@@ -33,6 +27,13 @@ app.set('views', path.join(__dirname, '../src/views'));
 // enable json message body for posting data to API
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+
+// DB Setup ------------------------
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/blog';
+mongoose.connect(mongoURI);
+// set mongoose promises to es6 default
+mongoose.Promise = global.Promise;
 
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
