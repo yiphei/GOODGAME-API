80599f661020669591b85bd7605e80fb38c0b704
savannah <savannah.m.liu.18@dartmouth.edu>
2018-05-21 22:26:02 -0400

    refactored post model and controller

--- src/models/post_model.js
+++ src/models/post_model.js
@@ -2,11 +2,16 @@ import mongoose, { Schema } from 'mongoose';
 
 // create a PostSchema with a title field
 const PostSchema = new Schema({
-  title: String,
-  tags: String,
-  content: String,
-  cover_url: String,
-  author: String, // author: { type: Schema.Types.ObjectId, ref: 'User' },
+  date: String, // date: { type: Date, default: Date.now },
+  time: String,
+  duration: Number, // minutes
+  lat: Number,
+  long: Number,
+  players_needed: Number,
+  max_players: Number,
+  level: Number,
+  players_list: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  creator: { type: Schema.Types.ObjectId, ref: 'User' },
 });
 
 // create PostModel class from schema
