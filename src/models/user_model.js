16324369421b31c9a0df47d592d527789a5e9977
Ally <ally.h.mahmoud.19@dartmouth.edu>
2018-05-29 21:27:23 -0400

    added score attribute to user model

--- src/models/user_model.js
+++ src/models/user_model.js
@@ -8,6 +8,7 @@ const UserSchema = new Schema({
   password: { type: String },
   handle: { type: String }, //   handle: { type: String, unique: true, lowercase: true },
   games: [{ type: Schema.Types.ObjectId, ref: 'Post' }], // games and status
+  score: Number,
 });
 
 // provides a mapping to id from _id,
